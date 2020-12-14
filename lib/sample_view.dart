a81aa07bdb151e0cff069a698b0890106c2aa36c
任跃兵 <cnryb@live.cn>
2020-12-14 21:55:40 +0800

    包括一下

--- lib/sample_view.dart
+++ lib/sample_view.dart
@@ -2,14 +2,22 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SampleView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    //使用Android平台的AndroidView，传入唯一标识符sampleView
+  
+  Widget _getNativeView(){
+//使用Android平台的AndroidView，传入唯一标识符sampleView
     if (defaultTargetPlatform == TargetPlatform.android) {
       return AndroidView(viewType: 'sampleView');
     } else {
       //使用iOS平台的UIKitView，传入唯一标识符sampleView
       return UiKitView(viewType: 'sampleView');
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('里面的那片红是原生绘制的'),),
+      body: _getNativeView(),
+    );
   }
 }
